f25f69e6ef652a61e75daef9009f6ee72361dc7e
unknown <SoumenBaral>
2023-01-04 02:52:28 +0600

    ServiceSection Done

--- src/Pages/Services/Service.js
+++ src/Pages/Services/Service.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Service = () => {
+const Service = ({ service }) => {
+    const { picture, name, about } = service
+    const navigate = useNavigate()
     return (
-        <div>
-            <h1>Service</h1>
+        <div className='border border-fuchsia-300 shadow-inner relative'>
+
+            <div className='flex justify-center mt-2' >
+                <img className='w-[75%] ' src={picture} alt="" />
+            </div>
+            <h1 className='text-center text-xl'>{name}</h1>
+            <p className='m-2'>{about}</p>
+            <div className='flex justify-end mr-4 p-2'>
+                <button onClick={() => navigate('/')} className='bg-blue-800 text-white border border-fuchsia-300 px-2 md:px-4 md:py-2 rounded shadow-xl hover:bg-blue-700'>CheckOut</button>
+            </div>
         </div>
     );
 };
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,9 +1,21 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import Service from './Service'
 const Services = () => {
+    const [services, setService] = useState([])
+    useEffect(() => {
+        fetch('Services.json')
+            .then(res => res.json())
+            .then(data => setService(data))
+    }, [])
     return (
-        <div>
-            <h1>Services</h1>
+        <div className='md:grid grid-cols-2 m-10 gap-5'>
+            
+            {
+                services.map(service => <Service
+                    key={service.id}
+                    service={service}
+                ></Service>)
+            }
         </div>
     );
 };
