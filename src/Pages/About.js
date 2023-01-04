7755d86fbc385febb78a76389465eec8a7b8db74
unknown <SoumenBaral>
2023-01-04 13:00:40 +0600

    About section Done!

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import profile from '../img/shape-9.png'
 const About = () => {
     return (
-        <div>
-            About
+        <div className='bg-slate-800'>
+            <div className='flex items-center'>
+                <img className='w-[40%] h-[75%]' src={profile} alt="" />
+                <div className='text-white name'>
+                    <h2 data-text='&nbsp;Soumen Baral Shuvo &nbsp;' className='  text-center '>&nbsp;Soumen Baral Shuvo &nbsp; </h2>
+                </div>
+            </div>
         </div>
     );
 };
